2b1a9e91b5027eee6b9fea748ca5186543b7c8e3
1jayyaj1 <jayabisaad@gmail.com>
2021-04-21 20:37:06 -0400

    Implemented camerax preview view use case

--- app/src/main/java/com/jayyaj/memoryeye/view/CameraFragment.java
+++ app/src/main/java/com/jayyaj/memoryeye/view/CameraFragment.java
@@ -2,15 +2,35 @@ package com.jayyaj.memoryeye.view;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.Preview;
+import androidx.camera.core.ViewPort;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.util.Log;
+import android.util.Size;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.jayyaj.memoryeye.R;
+import com.jayyaj.memoryeye.view.model.CameraXViewModel;
 
 public class CameraFragment extends Fragment {
+
+    private static final String TAG = "CameraFragment";
+    @Nullable private ProcessCameraProvider cameraProvider;
+    @Nullable private Preview previewUseCase;
+
+    private PreviewView previewView;
+    private int lensFacing = CameraSelector.LENS_FACING_FRONT;
+    private CameraSelector cameraSelector;
 
     public CameraFragment() {
         // Required empty public constructor
@@ -31,5 +51,54 @@ public class CameraFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_camera, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        cameraSelector = new CameraSelector.Builder().requireLensFacing(lensFacing).build();
+        previewView = view.findViewById(R.id.previewView);
+        if (previewView == null) {
+            Log.d(TAG, "previewView is null");
+        }
+
+        new ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory.getInstance(getActivity().getApplication()))
+                .get(CameraXViewModel.class)
+                .getProcessCameraProvider()
+                .observe(
+                        this,
+                        provider -> {
+                            cameraProvider = provider;
+                            bindAllCameraUseCases();
+                        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        bindAllCameraUseCases();
+    }
+
+    private void bindAllCameraUseCases() {
+        if (cameraProvider != null) {
+            cameraProvider.unbindAll();
+            bindPreviewUseCase();
+        }
+    }
+
+    private void bindPreviewUseCase() {
+        if (cameraProvider == null) {
+            return;
+        }
+        if (previewUseCase != null) {
+            cameraProvider.unbind(previewUseCase);
+        }
+
+        Preview.Builder builder = new Preview.Builder();
+        Size targetResolution = new Size(1200, 1600);
+        builder.setTargetResolution(targetResolution);
+        previewUseCase = builder.build();
+        previewUseCase.setSurfaceProvider(previewView.getSurfaceProvider());
+        cameraProvider.bindToLifecycle(this, cameraSelector, previewUseCase);
     }
 }
