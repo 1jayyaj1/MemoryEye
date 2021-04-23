bb12fec89c18f42da7c1dc4f49a5be5235c1a466
1jayyaj1 <jayabisaad@gmail.com>
2021-04-23 13:38:19 -0400

    Added initial version of repository

--- app/src/main/java/com/jayyaj/memoryeye/data/MemoryRepository.java
+++ app/src/main/java/com/jayyaj/memoryeye/data/MemoryRepository.java
@@ -1,17 +1,22 @@
 package com.jayyaj.memoryeye.data;
 
+import android.net.Uri;
+import android.util.Log;
+
+import com.google.firebase.Timestamp;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.StorageReference;
 import com.jayyaj.memoryeye.model.Memory;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MemoryRepository {
     private static final String TAG = "MemoryRepository";
     private static final FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
     private StorageReference storageReference;
-    private CollectionReference collectionReference = firebaseFirestore.collection("Journal");
+    private CollectionReference collectionReference = firebaseFirestore.collection("Memories");
 
 //    public List<Memory> getAllMemories() {}
 //
@@ -21,5 +26,29 @@ public class MemoryRepository {
 //
 //    public void deleteMemory(String memoryId) {}
 //
-//    public void createMemory(Memory memory) {}
+      public void createMemory(Memory memory) {
+          for (String image : memory.getImages()) {
+              Uri imageUri = Uri.parse(image);
+              StorageReference filePath = storageReference
+                      .child("memory_image")
+                      .child(memory.getMemoryId() + "_img_" + Timestamp.now().getSeconds());
+
+              filePath.putFile(imageUri).addOnSuccessListener(taskSnapshot -> {
+                  filePath.getDownloadUrl().addOnSuccessListener(uri -> {
+                      String imageUrl = uri.toString();
+                      //journal.setTimeAdded(new Timestamp(new Date()));
+
+                      collectionReference.add(memory).addOnSuccessListener(documentReference -> {
+                          Log.d(TAG, "Successfully created memory");
+                      }).addOnFailureListener(e -> {
+                          Log.e(TAG, "Could not create memory");
+                      });
+                  }).addOnFailureListener(e -> {
+                      Log.e(TAG, "Could not create memory");
+                  });
+              }).addOnFailureListener(e -> {
+                  Log.e(TAG, "Could not create memory");
+              });
+          }
+      }
 }
